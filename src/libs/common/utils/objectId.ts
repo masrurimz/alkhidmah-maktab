a3d83eb89433646372c04ebd342d376198038da7
Muhammad Zahid Masruri <zahid@hellonimbly.com>
2023-01-15 15:03:51 +0700

    Add default value to objectId

--- src/libs/common/utils/objectId.ts
+++ src/libs/common/utils/objectId.ts
@@ -1,4 +1,4 @@
-export const checkIsValidObjectId = (id: string): boolean => {
+export const checkIsValidObjectId = (id = ""): boolean => {
   const objectIdRegex = /^[0-9a-fA-F]{24}$/;
 
   return Number(id.match(objectIdRegex)?.length) > 0;
